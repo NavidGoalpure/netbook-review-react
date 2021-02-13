7bc1640bd8e784ee98762ec016fda807ad9cf6b8
navidgoalpure <navid.goalpure@gmail.com>
2021-02-13 11:57:43 +0330

    in progress

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,10 +1,9 @@
 import logo from "../../Static/Images/netbook-full-logo.png";
-import { ReactComponent as SearchIcon } from "../../Static/Images/ic24-search.svg";
 import "./style.css";
 const Header = () => {
   return (
     <header id="main-header">
-      <div className="menu-container">
+      <div className="menu-container clearfix">
         <div className="logo-container">
           <span className="logo-helper"></span>
           <a href="https://netbooksreview.net/">
@@ -46,9 +45,29 @@ const Header = () => {
             </ul>
           </nav>
           <div id="top_search">
-            <span id="search_icon">
-              <SearchIcon style={{ width: "17", height: "17px" }} />
-            </span>
+            <span id="search_icon"></span>
+          </div>
+          <div id="mobile_nav_menu">
+            <div className="mobile_nav closed">
+              <span className="select_page">Select Page</span>
+              <span className="mobile_menu_bar mobile_menu_bar_toggle"></span>
+              <ul id="mobile_menu" className="mobile_menu">
+                <li>
+                  <a href="https://netbooksreview.net/home3/">HOME</a>
+                </li>
+                <li>
+                  <a href="https://netbooksreview.net/about3/">ABOUT</a>
+                </li>
+                <li>
+                  <a href="https://netbooksreview.net/reviews3/">REVIEWS</a>
+                </li>
+                <li>
+                  <a href="https://www.officelessagency.com/begin?affiliate_id=2551017">
+                    <span className="pb_button">{`Start Earning`}</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
